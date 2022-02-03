0dd77fc6d5682b447226e8ea11026244f1b0fce0
spankyman125 <spankyman@live.ru>
2022-02-03 20:20:56 +0700

    Routing file's structure changed

---
+++ backend/app/routing/endpoints/song.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app import crud
+from app import schemas
+from app import dependencies
+
+router = APIRouter()
+
+@router.get("/{id}", response_model=schemas.SongRead)
+def read_song(id: int, db: Session = Depends(dependencies.get_db)):
+    song = crud.get_song(db, song_id=id)
+    if song is None:
+        raise HTTPException(status_code=404, detail='Song not found')
+    return song
+
+@router.get("/", response_model=List[schemas.SongRead])
+def read_songs(skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db)):
+    songs = crud.get_songs(db, skip=skip, limit=limit)
+    return songs
