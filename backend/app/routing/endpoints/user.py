89d64c5be75a9ba05aaca563ee795dff9fd371f4
spankyman125 <spankyman@live.ru>
2022-02-11 15:52:52 +0700

    User's favourite artists endpoint's logic changed

--- backend/app/routing/endpoints/user.py
+++ backend/app/routing/endpoints/user.py
@@ -1,4 +1,5 @@
 from typing import List
+from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -12,27 +13,39 @@ from app import models
 
 router = APIRouter()
 
-@router.get("/me", response_model=schemas.UserAll)
-def return_current_user(current_user: models.User = Depends(dependencies.get_current_user)):
-    return current_user
-
-@router.get("/me/artists", response_model=List[schemas.ArtistBase])
-def read_artists_liked(skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db), current_user: models.User = Depends(dependencies.get_current_user)):
-    artists = crud_artist.get_artists_liked(db, skip=skip, limit=limit, user=current_user)
-    return artists
-
 @router.get("/{username}/artists", response_model=List[schemas.ArtistBase])
-def read_artists_liked(username: str ,skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db), current_user: models.User = Depends(dependencies.get_current_user)):
-    db_user = crud_user.get_user(db, username=username)
-    artists = crud_artist.get_artists_liked(db, skip=skip, limit=limit, user=db_user)
+@router.get("/me/artists", response_model=List[schemas.ArtistBase])
+def read_favourite_artists(
+    username: Optional[str] = None,
+    skip: int = 0, 
+    limit: int = 100, 
+    db: Session = Depends(dependencies.get_db), 
+    current_user: models.User = Depends(dependencies.get_current_user)
+    ):
+    if username:
+        db_user = crud_user.get_user(db, username=username)
+        if db_user is None:
+            raise HTTPException(status_code=404, detail='User not found')
+        artists = crud_artist.get_artists_liked(db, skip=skip, limit=limit, user=db_user)
+    else:
+        artists = crud_artist.get_artists_liked(db, skip=skip, limit=limit, user=current_user)
     return artists
+
 
 @router.get("/{username}", response_model=schemas.UserAll)
-def read_user(username: str, db: Session = Depends(dependencies.get_db)):
-    user = crud_user.get_user(db, username=username)
-    if user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-    return user
+@router.get("/me", response_model=schemas.UserAll)
+def read_user(
+    username: Optional[str] = None, 
+    db: Session = Depends(dependencies.get_db),
+    current_user: models.User = Depends(dependencies.get_current_user)
+    ):
+    if username:
+        user = crud_user.get_user(db, username=username)
+        if user is None:
+            raise HTTPException(status_code=404, detail='User not found')
+        return user
+    else:
+        return current_user
 
 @router.post("/", response_model=schemas.UserAll)
 def create_user(user: schemas.UserCreate, db: Session = Depends(dependencies.get_db)):
