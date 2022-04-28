92c8249dfa4c54b147e0f089ffe3a4260a486620
spankyman125 <spankyman@live.ru>
2022-04-28 14:47:12 +0700

    Token expire response status code added

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -24,6 +24,11 @@ async def get_current_user(db: Session = Depends(get_db), token: str = Depends(o
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    token_expire_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token expired",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
         payload = jwt.decode(token, security.SECRET_KEY, algorithms=[security.ALGORITHM])
         username: str = payload.get("sub")
@@ -31,7 +36,7 @@ async def get_current_user(db: Session = Depends(get_db), token: str = Depends(o
             raise credentials_exception
         token_data = schemas.TokenData(username=username)
     except JWTError:
-        raise credentials_exception
+        raise token_expire_exception
     user = crud_user.get_user(db, username=token_data.username)
     if user is None:
         raise credentials_exception
