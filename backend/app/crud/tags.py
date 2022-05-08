4cf53c8431b6b9e68dc5b76657f59b0acfb8585c
spankyman125 <spankyman@live.ru>
2022-05-08 13:48:08 +0600

    Tags fixed

--- backend/app/crud/tags.py
+++ backend/app/crud/tags.py
@@ -11,16 +11,19 @@ class TagCRUD():
         return "by_name"
     
     def get_all(self, db: Session):
-        return "all"
+        tags = db.query(models.Tag).all()
+        return tags
 
     def get_songs(self, db: Session, tag: str):
         return "songs_by_tag"
 
-    def get_songs(self, db: Session, tags: List[str]):
+    def get_songs(self, db: Session, tags: List[str], skip: int = 0, limit: int = 100):
         songs = db.query(models.Song).\
             options(joinedload(models.Song.tags)).\
-            filter(models.Tag.name.in_(tags)).all()
-            # join(models.Song.tags).\
+            filter(models.Tag.name.in_(tags)).\
+            offset(skip).\
+            limit(limit).\
+            all()
         return songs
 
 crud_tag = TagCRUD()
