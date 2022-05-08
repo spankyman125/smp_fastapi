9271729c2aff1aa6c0128375846aed51428256c8
spankyman125 <spankyman@live.ru>
2022-05-08 13:48:08 +0600

    Tags fixed

--- backend/app/crud/tags.py
+++ backend/app/crud/tags.py
@@ -1,7 +1,8 @@
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session, joinedload, lazyload
 
 from app import models, schemas
 from typing import Optional, List
+from app.dependencies import add_like_attr
 
 class TagCRUD():
     def get(self, db: Session, id: int):
@@ -11,16 +12,22 @@ class TagCRUD():
         return "by_name"
     
     def get_all(self, db: Session):
-        return "all"
+        tags = db.query(models.Tag).all()
+        return tags
 
     def get_songs(self, db: Session, tag: str):
         return "songs_by_tag"
 
-    def get_songs(self, db: Session, tags: List[str]):
+    def get_songs(self, db: Session, tags: List[str], skip: int = 0, limit: int = 100, current_user: Optional[schemas.User] = None):
         songs = db.query(models.Song).\
-            options(joinedload(models.Song.tags)).\
-            filter(models.Tag.name.in_(tags)).all()
-            # join(models.Song.tags).\
+            filter(models.Song.tags.any(models.Tag.name.in_(tags))).\
+            offset(skip).\
+            limit(limit).\
+            all()
+        if current_user:
+            current_db_user = db.query(models.User).filter(models.User.username == current_user.username).first()
+            for i in range(len(songs)):
+                add_like_attr(current_db_user, [songs[i]], "songs")
         return songs
 
 crud_tag = TagCRUD()
--- backend/app/routing/endpoints/tags.py
+++ backend/app/routing/endpoints/tags.py
@@ -17,11 +17,12 @@ def get_tags(
     ):
     return crud_tag.get_all(db)
 
-# @router.get("/songs", response_model=List[schemas.SongLoaded])
-@router.get("/songs")
+@router.get("/songs", response_model=List[schemas.SongTagged])
 def get_songs_by_tags(
+        skip: int = 0, 
+        limit: int = 100, 
         tags: List[str]=Query([]),
         db: Session = Depends(dependencies.get_db),
         current_user: schemas.User = Depends(dependencies.get_current_user_optional),
     ):
-    return crud_tag.get_songs(db=db, tags=tags)
+    return crud_tag.get_songs(db=db, skip=skip, limit=limit, tags=tags, current_user=current_user)
